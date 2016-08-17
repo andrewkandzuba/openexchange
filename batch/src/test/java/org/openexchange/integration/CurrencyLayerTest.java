8e14f981d72cf19bb98132b12c568917007ebc60
andriyka <andriyka@playtech.com>
2016-08-17 16:01:35 +0300

    all unit tests works

--- batch/src/test/java/org/openexchange/integration/CurrencyLayerTest.java
+++ batch/src/test/java/org/openexchange/integration/CurrencyLayerTest.java
@@ -1,27 +1,36 @@
 package org.openexchange.integration;
 
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
+import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(locations = "classpath:test.properties")
 public class CurrencyLayerTest {
-    @Autowired
+    @InjectMocks
     private CurrencyLayerServiceImpl currencyService;
-
+    @MockBean
+    private RestTemplate restTemplate;
     @Before
     public void setUp() throws Exception {
         initMocks(this);
@@ -29,15 +38,32 @@ public class CurrencyLayerTest {
 
     @Test
     public void testCurrencyList() throws Exception {
-        Currencies currencies = currencyService.all();
-        Assert.assertNotNull(currencies);
-        Assert.assertEquals(168, currencies.getCurrencies().size());
+        Currencies currencies = new Currencies();
+        currencies.setSuccess(true);
+        currencies.setCurrencies(Map.of("USD", "United States Dollar"));
+
+        when(restTemplate.getForEntity(Mockito.anyString(), Mockito.any(),  Matchers.<Object>anyVararg())).thenReturn(new ResponseEntity<>(currencies, HttpStatus.OK));
+
+        Currencies resp = currencyService.all();
+        Assert.assertNotNull(resp);
+        Assert.assertTrue(resp.getSuccess());
+        Assert.assertEquals(1, currencies.getCurrencies().size());
+        Assert.assertEquals("United States Dollar", resp.getCurrencies().get("USD"));
     }
 
     @Test
     public void testQuotes() throws Exception {
-        Quotes quotes = currencyService.live(Arrays.asList("USD", "EUR"));
+        Quotes quotes = new Quotes();
+        quotes.setSuccess(true);
+        quotes.setSource("USD");
+        quotes.setQuotes(Map.of("USDUSD", 1.00, "USDEUR", 0.90));
+        List<String> params = Arrays.asList("USD", "EUR");
+
+        when(restTemplate.getForEntity(Mockito.anyString(), Mockito.any(),  Matchers.<Object>anyVararg())).thenReturn(new ResponseEntity<>(quotes, HttpStatus.OK));
+
+        Quotes resp = currencyService.live(params);
         Assert.assertNotNull(quotes);
+        Assert.assertTrue(resp.getSuccess());
         Assert.assertEquals("USD", quotes.getSource());
         Assert.assertEquals(2, quotes.getQuotes().size());
     }
